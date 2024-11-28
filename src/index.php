b4edc883fecfa11b72df0a2d26ee217bcfd7f34e
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2024-11-28 22:14:47 +0100

    empezando_el_Index

--- src/index.php
+++ src/index.php
@@ -1,5 +1,15 @@
 <?php
-//require_once 'php/vistas/registro.php';
-//require_once 'php/vistas/registroAdmin.php';
-//require_once 'php/vistas/inicioSesion.html';
-//require_once 'php/vistas/inicioSesionAdmin.html';
+    require_once 'php/config/config.php';
+
+    //   Asignamos automaticamente el metodo y controlador por defecto si no le pasas ninguno.
+    if(!isset($_GET['controlador']))$_GET['controlador'] = controlador_Usuario;
+    if(!isset($_GET['metodo'])){$_GET['metodo'] = metodo_Usuario;}
+
+    //Concatenamos para la ruta del controlador e incluirlo despues
+    $rutaControlador = 'php/controladores/c' . $_GET['controlador'] . '.php';
+    require_once $rutaControlador;
+
+    //Creamos el objeto del controlador.
+    $controlador = 'C'.$_GET['controlador'];
+    $objControlador = new $controlador();
+    
