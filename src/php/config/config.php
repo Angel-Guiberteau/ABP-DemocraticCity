a7da1557d862c898e8ea7e8ca632a218fc8b5e29
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2024-11-29 18:23:01 +0100

    mejora_Controlador_Modelo

---
+++ src/php/config/config.php
@@ -0,0 +1,17 @@
+<?php
+// VISTAR
+
+define("VISTA_INICIAL", "inicioSesion");
+
+define("VISTA_INICIO_ADMIN","inicioSesionAdmin");
+
+define("VISTA_REGISTRO","registro.php");
+
+define("VISTA_REGISTRO_ADMIN","registroAdmin");
+
+
+// CONTROLADORES y MODELOS
+
+define("CONTROLADOR_USUARIO","Usuario");
+
+define("METODO_DEFAULT","__construct");
