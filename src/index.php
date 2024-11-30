b5ae90c49d1cea9fb3a69893ce159717f85f4d50
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2024-11-30 19:58:06 +0100

    pequeños_cambios

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@
 
     //   Asignamos automaticamente el metodo y controlador por defecto si no le pasas ninguno.
     if(!isset($_GET['c']))$_GET['c'] = controlador_Usuario;
-    if(!isset($_GET['a'])){$_GET['a'] = '';} // sin metodo por defecto.
+    if(!isset($_GET['m'])){$_GET['m'] = '';} // sin metodo por defecto.
 
     //Concatenamos para la ruta del controlador e incluirlo despues
-    $rutaControlador = 'php/controladores/c' . $_GET['c'] . '.php';
+    $rutaControlador = 'php/controladores/c' . $_GET['m'] . '.php';
     require_once $rutaControlador;
 
     //Creamos el objeto del controlador.
@@ -16,8 +16,8 @@
 
     $datos;
     if($_SERVER['REQUEST_METHOD']=='POST')// Compueba se ha enviado un $_POST y realiza una cosa u otra en funcion de lo que necesite.
-        if(method_exists($objControlador, $_GET['a']))$datos = $objControlador->{$_GET['a']}($_POST);
+        if(method_exists($objControlador, $_GET['m']))$datos = $objControlador->{$_GET['m']}($_POST);
     else
-        if(method_exists($objControlador, $_GET['a']))$datos = $objControlador->{$_GET['a']}();
+        if(method_exists($objControlador, $_GET['m']))$datos = $objControlador->{$_GET['m']}();
 
     require_once 'php/vistas/'.$objControlador->vista.'.php';
