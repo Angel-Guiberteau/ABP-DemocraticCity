a6398f20cc6cf8c681b7625660727b2d5faa4e23
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2024-12-02 13:21:54 +0100

    Añadido boton musica funcional Registro

---
+++ src/js/musica.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const boton = document.getElementById('musica'); // Obtiene el elemento de audio.
+    const botonMute = document.getElementById('musica2'); // Obtiene el botón de silencio.
+    boton.addEventListener('click', function () { // Agrega un evento al hacer clic en el botón.
+        const audio = document.getElementById('audio'); // Obtiene el elemento de audio.
+        audio.play();
+        botonMute.style.display = 'inline'; // Muestra el botón de silencio.
+        boton.style.display = 'none'; // Muestra el botón de silencio.
+    });
+
+    botonMute.addEventListener('click', function () { // Agrega un evento al hacer clic en el botón.
+        const audio = document.getElementById('audio'); // Obtiene el elemento de audio.
+        audio.pause();
+        boton.style.display = 'inline'; // Muestra el botón de silencio.
+        botonMute.style.display = 'none'; // Muestra el botón de silencio.
+    });
+});
