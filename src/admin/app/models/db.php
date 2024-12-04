cf2b0100fdf9c5fe8a188ea0ac1e5696a784af74
Fernando José González Bernáldez <ferferjose99@gmail.com>
2024-12-04 09:07:51 +0100

    Pdo hecho con rutas?

--- src/admin/app/models/db.php
+++ src/admin/app/models/db.php
@@ -6,7 +6,7 @@ class Db{
     private $bbdd;
     public $conexion;
     function __construct(){
-        require_once 'php/config/configDB.php';
+        require_once RUTA_CONFIGS.'.configDB.php';
         $this->servidor = constant('SERVIDOR');
         $this->usuario = constant('USUARIO');
         $this->password = constant('PASSWORD');
