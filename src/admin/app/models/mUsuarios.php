45223785a95e9ef5cd58c4151c1e79b7a47a3f00
Fernando José González Bernáldez <ferferjose99@gmail.com>
2024-12-04 09:04:08 +0100

    Pdo hecho

--- src/admin/app/models/mUsuarios.php
+++ src/admin/app/models/mUsuarios.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Esta clase permite la total funcionalidad de todos los procesos de los usuarios y administradores, sea inicio de sesión o registro.
+ */
 class MUsuarios{
     private $conexion;
     function __construct(){
@@ -6,66 +9,106 @@ class MUsuarios{
         $objConexion = new Db();
         $this->conexion= $objConexion->conexion;
     }
-    // registro de usuarios
+    /**
+     * Método que permite guardar en la bbdd el usuario.
+     * @param
+     */
     public function registrar($datos){
         try{
-            $sql="INSERT INTO Usuarios(nombreUsuario, passUsuario) VALUES('".$datos['usuario']."', '".$datos["passw"]."');";
+            // $sql = "INSERT INTO Usuarios(nombreUsuario, passUsuario) VALUES('".$datos['usuario']."', '".$datos["passw"]."');";
+            $sql = "INSERT INTO Usuarios(nombreUsuario, passUsuario) VALUES(:usuario, :passw);";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindValue(':usuario', $datos['usuario'], PDO::PARAM_STR);
+            $stmt->bindValue(':passw', $datos['passw'], PDO::PARAM_STR);
 
-            $this->conexion -> query($sql);
+            $stmt->execute();
 
-            return $this->conexion->affected_rows > 0;
+            return $stmt->rowCount() > 0;
 
-        }catch (mysqli_sql_exception $e){
+        }catch (PDOException $e) {
+            error_log("Error en la consulta: " . $e->getMessage());
             return false;
         }
     }
+    /**
+     * Método que permite registrar nuevos Admin (Solo puede hacerlo un SUPERAdmin)
+     * @param
+     */
     public function registrarAdm($datos){
         try{
 
-            $sql="INSERT INTO Administradores(nombreUsuario, passAdmin, superAdmin) VALUES('".$datos['usuario']."', '".$datos["passw"]."', 0);";
+            $sql="INSERT INTO Administradores(nombreUsuario, passAdmin, superAdmin) VALUES(:usuarios, :passw, 0);";
 
-            $this->conexion->query($sql);
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindValue(':usuario', $datos['usuario'], PDO::PARAM_STR);
+            $stmt->bindValue(':passw', $datos['passw'], PDO::PARAM_STR);
 
-            return $this->conexion->affected_rows > 0;
+            $stmt->execute();
 
-        }catch (mysqli_sql_exception $e){
+            return $stmt->rowCount() > 0;
+
+        }catch (PDOException $e) {
+            error_log("Error en la consulta: " . $e->getMessage());
             return false;
         }
     }
-    //inicio de sesion
+
+    /**
+     * Método que permite e inicio de sesión de los usuarios.
+     * @param
+     * @return
+     */
     public function inicio($datos){
         try{
-            $sql='SELECT * from Usuarios where nombreUsuario = "'.$datos["usuario"].'";';
-            $resultado = $this->conexion->query($sql);
-            $datos["passw"] = mysqli_real_escape_string($this->conexion, $datos["passw"]);
-            if($this->conexion->affected_rows > 0){
-                $fila=$resultado->fetch_assoc();
+
+            $sql='SELECT * from Usuarios where nombreUsuario = :usuario;';
+
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindValue(':usuario', $datos['usuario'], PDO::PARAM_STR);
+
+            $stmt->execute();
+            if($stmt->rowCount() > 0){
+                $fila = $stmt->fetch(PDO::FETCH_ASSOC);
                 if($datos["usuario"] == $fila['nombreUsuario'] && password_verify($datos["passw"], $fila["passUsuario"]))
                     return $fila;
                 else
                     return false;
             }
+                
             
-        }catch (mysqli_sql_exception $e){
+
+        }catch (PDOException $e) {
+            error_log("Error en la consulta: " . $e->getMessage());
             return false;
         }
     }
+    /**
+     * Método que permite el inicio de sesión de los administradores ya sean generales o SUPER.
+     * @param
+     * @return
+     */
     public function inicioAdm($datos){
+            
         try{
-            $sql='SELECT * from Administradores where nombreUsuario = "'.$datos["usuario"].'";';
-            $resultado = $this->conexion->query($sql);
-            $datos["passw"] = mysqli_real_escape_string($this->conexion, $datos["passw"]);
-            if($this->conexion->affected_rows > 0){
-                $fila=$resultado->fetch_assoc();
 
-                if(password_verify($datos["passw"], $fila["passAdmin"])){
+            $sql='SELECT * from Administradores where nombreUsuario = :usuario;';
+
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindValue(':usuario', $datos['usuario'], PDO::PARAM_STR);
+
+            $stmt->execute();
+            if($stmt->rowCount() > 0){
+                $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($datos["usuario"] == $fila['nombreUsuario'] && password_verify($datos["passw"], $fila["passUsuario"]))
                     return $fila;
-                }
                 else
                     return false;
             }
+                
             
-        }catch (mysqli_sql_exception $e){
+
+        }catch (PDOException $e) {
+            error_log("Error en la consulta: " . $e->getMessage());
             return false;
         }
     }
--- src/game/app/models/db.php
+++ src/game/app/models/db.php
@@ -6,11 +6,21 @@ class Db{
     private $bbdd;
     public $conexion;
     function __construct(){
-        require_once RUTA_CONFIGS.'configDB.php';
+        require_once 'php/config/configDB.php';
         $this->servidor = constant('SERVIDOR');
         $this->usuario = constant('USUARIO');
         $this->password = constant('PASSWORD');
         $this->bbdd = constant('BBDD');
-        $this->conexion = $this->conexion = new mysqli($this->servidor, $this->usuario, $this->password, $this->bbdd);
+
+        $dsn = 'mysql:host=' . $this->servidor . ';dbname=' . $this->bbdd;
+
+        $opciones = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+        try {
+            $this->conexion = new PDO($dsn, $this->usuario, $this->password, $opciones);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+        }
     }
 }
