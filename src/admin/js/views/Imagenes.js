bb576fc43651055232f665d3b81fbe12e79380ba
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2024-12-13 09:28:16 +0100

    la parte de imagenes realizada

--- src/admin/js/views/Imagenes.js
+++ src/admin/js/views/Imagenes.js
@@ -1,12 +1,14 @@
-import { CImagenes } from "../controllers/cImagenes";
+import { CImagenes } from "../../js/controllers/cImagenes.js";
 const objCImagenes = new CImagenes();
+
 document.getElementById("formularioImagenes").addEventListener("submit", async (event) => {
+
     event.preventDefault(); // Evitar que el formulario se envíe.
+
     alert("Entra en la vista de JS");
 
     const imagen = document.getElementById("imagenes");
-    console.log(imagen);
-    const archivo = imagen.files[0]; // Obtener el primer archivo.
+    const archivo = imagen.files[0]; // Obtener el primer archivo, por si consiguen meter mas de un archivo.
 
     // Comprobamos si se ha seleccionado un archivo.
     if (!archivo) {
@@ -21,13 +23,14 @@ document.getElementById("formularioImagenes").addEventListener("submit", async (
         return;
     }
 
-    // Crear un nuevo nombre para la imagen
-    const nombreNuevo = "imagen_" + new Date().getTime() + "." + archivo.name.split(".").pop();
+    objCImagenes.prepararImagen(archivo);
+    
+    /*// Crear un nuevo nombre para la imagen
+    //const nombreNuevo = "imagen_" + new Date().getTime() + "." + archivo.name.split(".").pop();
 
     // Crear un objeto FormData para enviar el archivo al servidor.
     const formData = new FormData();
     formData.append("imagen", archivo, nombreNuevo); // Añadir el archivo con el nuevo nombre.
-
     // Enviar el archivo al servidor usando fetch.
     try {
         const response = await fetch("src/admin/img", { // <-- aqui va la ruta del servidor.
@@ -43,5 +46,5 @@ document.getElementById("formularioImagenes").addEventListener("submit", async (
     } catch (error) {
         console.error("Error al enviar la imagen:", error);
         alert("Hubo un problema al enviar la imagen.");
-    }
+    }*/
 });
