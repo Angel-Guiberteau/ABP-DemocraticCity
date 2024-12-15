318cc29fe5ca9b495d1d219c5e850f3155fba2ff
Fernando José González Bernáldez <ferferjose99@gmail.com>
2024-12-15 23:11:09 +0100

    Modificacion de estilo

--- src/game/app/views/juegoAnfitrion.php
+++ src/game/app/views/juegoAnfitrion.php
@@ -13,8 +13,11 @@
         echo '<input type="hidden" id="nombreCiudadOculto" value="'.$_SESSION['nombreCiudad'].'">';
     ?>
     <div id="juego">
-        <nav class="inicio">
-            <button><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg></button>
+        <audio src="audio/musicaInicio.mp3" preload="auto" id="audio"></audio>
+
+        <nav class="icons">
+            <button id="musica" class="unmute"><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg></button>
+            <button id="musica2" class="mute"><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464" /><path d="M17.7 5a9 9 0 0 1 2.362 11.086m-1.676 2.299a9 9 0 0 1 -.686 .615" /><path d="M9.069 5.054l.431 -.554a.8 .8 0 0 1 1.5 .5v2m0 4v8a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l1.294 -1.664" /><path d="M3 3l18 18" /></svg></button>
         </nav>
         <div class="preguntas">
             <?php
@@ -32,27 +35,14 @@
                 echo '<div class="medidores">';
                     echo '<button id="economia">Economia: <span id="valorEconomia">5</span></button>';
                     echo '<button id="sanidad">Sanidad: <span id="valorSanidad">5</span></button>';
-                    echo '<button id="seguridad">Seguridad <span id="valorSeguridad">5</span></button>';
+                    echo '<button id="seguridad">Seguridad: <span id="valorSeguridad">5</span></button>';
                     echo '<button id="educacion">Educacion: <span id="valorEducacion">5</span></button>';
                 echo '</div>';
             ?>
             <div class="mapa">
+                <img src="img/mapaJuego.png" alt="">
                 <div id="edificio1"></div>
                 <div id="edificio2"></div>
-                <div id="edificio3"></div>
-                <div id="edificio4"></div>
-                <div id="edificio5"></div>
-                <div id="edificio6"></div>
-                <div id="edificio7"></div>
-                <div id="edificio8"></div>
-                <div id="edificio9"></div>
-                <div id="edificio10"></div>
-                <div id="edificio11"></div>
-                <div id="edificio12"></div>
-                <div id="edificio13"></div>
-                <div id="edificio14"></div>
-                <div id="edificio15"></div>
-                <div id="edificio16"></div>
             </div>
         </div>
     </div>
--- src/game/app/views/juegoUser.php
+++ src/game/app/views/juegoUser.php
@@ -13,8 +13,11 @@
         echo '<input type="hidden"  id="nombreCiudadOculto" value="'.$_SESSION['nombreCiudad'].'">';
     ?>
     <div id="juego">
-        <nav class="inicio">
-            <button><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg></button>
+    <audio src="audio/musicaInicio.mp3" preload="auto" id="audio"></audio>
+
+    <nav class="icons">
+            <button id="musica" class="unmute"><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg></button>
+            <button id="musica2" class="mute"><svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464" /><path d="M17.7 5a9 9 0 0 1 2.362 11.086m-1.676 2.299a9 9 0 0 1 -.686 .615" /><path d="M9.069 5.054l.431 -.554a.8 .8 0 0 1 1.5 .5v2m0 4v8a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l1.294 -1.664" /><path d="M3 3l18 18" /></svg></button>
         </nav>
         <div class="preguntas">
             <?php
@@ -32,7 +35,7 @@
                 echo '<div class="medidores">';
                     echo '<button id="economia">Economia: <span id="valorEconomia">5</span></button>';
                     echo '<button id="sanidad">Sanidad: <span id="valorSanidad">5</span></button>';
-                    echo '<button id="seguridad">Seguridad <span id="valorSeguridad">5</span></button>';
+                    echo '<button id="seguridad">Seguridad: <span id="valorSeguridad">5</span></button>';
                     echo '<button id="educacion">Educacion: <span id="valorEducacion">5</span></button>';
                 echo '</div>';
             ?>
@@ -43,7 +46,7 @@
         <img src="img/mapaJuego.png" alt="">
             <div id="edificio1"></div>
             <div id="edificio2"></div>
-        </div>
+    </div>
     <!-- MODAL INICIO JUEGO -->
     <div id="modalInicioJuego">
         <div class="modal-content">
@@ -93,6 +96,7 @@
     </div>
 
 
+    <script type="module" src="js/views/musica.js"></script>
     <script type="module" src="js/views/mostrarPreguntaUsuario.js"></script>
     <script type="module" src="js/controllers/cPartida.js"></script>
     <script type="module" src="js/models/mPartida.js"></script>
