1d05df588659e268ef977ac20b148f5fed2fff5d
Fernando José González Bernáldez <ferferjose99@gmail.com>
2024-12-16 00:00:58 +0100

    Easter egss

--- src/game/js/views/decirNombre.js
+++ src/game/js/views/decirNombre.js
@@ -1,10 +1,21 @@
 function hablarNombre() {
     let speech = '';
     const nombreUsuario = document.getElementById('parrafoNombreUsuario').textContent; // Obtener el texto
+    speech = new SpeechSynthesisUtterance(nombreUsuario);
     if(nombreUsuario == 'Cebepuche' || nombreUsuario == 'cebepuche'){
         speech = new SpeechSynthesisUtterance('¡¡¡¡¡CEBEPUTEROOO!!!!!!');
     }else{
-        speech = new SpeechSynthesisUtterance(nombreUsuario); // Crear la instancia de voz
+        if(nombreUsuario == 'Mauri' || nombreUsuario == 'mauri')
+            speech = new SpeechSynthesisUtterance('MAURICIO COLMENERO EL MÁS ALTO DE ESPAÑA');
+        else
+            if(nombreUsuario == 'Angel' || nombreUsuario == 'angel')
+                speech = new SpeechSynthesisUtterance('JULIA DONDE ESTÁS');
+            else
+                if(nombreUsuario == 'Joaquin' || nombreUsuario == 'joaquin')
+                    speech = new SpeechSynthesisUtterance('Angel estoy enamorado de ti');
+                else
+                    if(nombreUsuario == 'Alro' || nombreUsuario == 'alro')
+                        speech = new SpeechSynthesisUtterance('LA TUYA MÁS');
     }
     speech.lang = 'es-ES'; // Configura el idioma (español)
     speech.pitch = 1; // Tono de voz
